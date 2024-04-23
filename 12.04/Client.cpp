31cb258b31d7baa5fd7679c808364640e2064912
6eeesh <144094732+6eeesh@users.noreply.github.com>
2024-04-23 21:51:51 +0300

    Добавьте файлы проекта.

---
+++ 12.04/Client.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+#include <thread>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+
+using namespace std;
+
+const int PORT = 8888;
+const string SERVER = "127.0.0.1";
+
+void send_messages(int socket) {
+    string msg;
+    while (true) {
+        getline(cin, msg);
+        if (msg == "/exit") {
+            break;
+        }
+        send(socket, msg.c_str(), msg.size() + 1, 0);
+    }
+}
+
+void receive_messages(int socket) {
+    char buffer[1024];
+    int bytesread;
+    while ((bytesread = recv(socket, buffer, 1024, 0)) > 0) {
+        buffer[bytesread] = 0;
+        cout << buffer << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+#ifdef _WIN32
+    WSAData wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+#endif
+
+    int clientsocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientsocket == -1) {
+        cerr << "Ошибка создания сокета!" << endl;
+        return 1;
+    }
+
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(SERVER.c_str());
+    addr.sin_port = htons(PORT);
+
+    if (connect(clientsocket, (sockaddr*)&addr, sizeof(addr)) == -1) {
+        cerr << "Ошибка подключения к серверу!" << endl;
+        return 1;
+    }
+
+    cout << "Подключение к серверу успешно!" << endl;
+
+    thread sender(send_messages, clientsocket);
+    thread receiver(receive_messages, clientsocket);
+
+    sender.join();
+    receiver.join();
+
+    closesocket(clientsocket);
+
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,162 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <set>
+#include <fstream>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+
+using namespace std;
+
+const int PORT = 8888;
+const int MAXCLIENTS = 10;
+const string HISTORY_FILE = "chat_history.txt";
+
+typedef vector<string> msg_history;
+typedef set<int> connected_clients;
+typedef struct {
+    int socket;
+    thread* threadptr;
+} client_info;
+
+msg_history messages;
+connected_clients clients;
+mutex mtx;
+
+void load_history() {
+    ifstream file(HISTORY_FILE);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            messages.push_back(line);
+        }
+        file.close();
+    }
+}
+
+void save_history() {
+    ofstream file(HISTORY_FILE);
+    if (file.is_open()) {
+        for (const string& msg : messages) {
+            file << msg << endl;
+        }
+        file.close();
+    }
+}
+
+void broadcast_message(const string& msg, int sender) {
+    for (int c : clients) {
+        if (c != sender) {
+            send(c, msg.c_str(), msg.size() + 1, 0);
+        }
+    }
+}
+
+void broadcast_notification(const string& notification) {
+    for (int c : clients) {
+        send(c, notification.c_str(), notification.size() + 1, 0);
+    }
+}
+
+void client_handler(int clientsocket) {
+    char buffer[1024];
+    int bytesread;
+
+    mtx.lock();
+    for (const string& msg : messages) {
+        send(clientsocket, msg.c_str(), msg.size() + 1, 0);
+    }
+    mtx.unlock();
+
+    string notification = "Новый клиент подключился!";
+    broadcast_notification(notification);
+
+    while ((bytesread = recv(clientsocket, buffer, 1024, 0)) > 0) {
+        buffer[bytesread] = 0;
+        string msg(buffer);
+
+        mtx.lock();
+        messages.push_back(msg);
+        broadcast_message(msg, clientsocket);
+        save_history();
+        mtx.unlock();
+    }
+
+    mtx.lock();
+    clients.erase(clientsocket);
+    notification = "Клиент отключился!";
+    broadcast_notification(notification);
+    mtx.unlock();
+
+    closesocket(clientsocket);
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+#ifdef _WIN32
+    WSAData wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+#endif
+
+    int serversocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serversocket == -1) {
+        cerr << "Ошибка создания сокета!" << endl;
+        return 1;
+    }
+
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+
+    if (bind(serversocket, (sockaddr*)&addr, sizeof(addr)) == -1) {
+        cerr << "Ошибка привязки сокета!" << endl;
+        return 1;
+    }
+
+    if (listen(serversocket, MAXCLIENTS) == -1) {
+        cerr << "Ошибка прослушивания сокета!" << endl;
+        return 1;
+    }
+
+    cout << "Сервер запущен на порту " << PORT << endl;
+
+    load_history();
+
+    while (true) {
+        sockaddr_in clientaddr;
+        int addrsize = sizeof(clientaddr);
+        int clientsocket = accept(serversocket, (sockaddr*)&clientaddr, &addrsize);
+        if (clientsocket == -1) {
+            cerr << "Ошибка принятия подключения!" << endl;
+            continue;
+        }
+
+        mtx.lock();
+        clients.insert(clientsocket);
+        mtx.unlock();
+
+        client_info* ci = new client_info;
+        ci->socket = clientsocket;
+        ci->threadptr = new thread(client_handler, clientsocket);
+    }
+
+    save_history();
+
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    return 0;
+}
